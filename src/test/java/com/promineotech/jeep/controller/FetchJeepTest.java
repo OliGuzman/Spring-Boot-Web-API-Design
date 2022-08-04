01eb101d0a718984e7d429e5aea38198728c5d2a
OliGuzman <OlGuDev@gmail.com>
2022-08-04 18:28:57 -0500

    first commit

--- src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
+++ src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
+
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,9 +16,13 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.jdbc.JdbcTestUtils;
+
+import com.promineotech.jeep.controller.support.FetchJeepTestSupport;
 import com.promineotech.jeep.entity.Jeep;
 import com.promineotech.jeep.entity.JeepModel;
 import lombok.Getter;
@@ -27,14 +33,15 @@ import lombok.Getter;
 		"classpath:flyway/migrations/V1.0__Jeep_Schema.sql",
 		"classpath:flyway/migrations/V1.1__Jeep_Data.sql"},
 		config = @SqlConfig(encoding = "utf-8"))
-class FetchJeepTest {
+class FetchJeepTest extends FetchJeepTestSupport {
+	
 	@Autowired
 	@Getter
 	private TestRestTemplate restTemplate; 
 	
 	@LocalServerPort
 	private int serverPort; 
-
+	
 	@Test
 	void testThatJeepsAreReturnedWhenAValidModelAndTrimAreSupplied() {
 		//Given: a valid model, trim and URI
@@ -44,9 +51,17 @@ class FetchJeepTest {
 				String.format("http://localhost:%d/jeeps?model=%s&trim=%s", serverPort, model, trim); 
 		
 		//When: a connection is made to the URI		
-		ResponseEntity<List<Jeep>> response = restTemplate.exchange(uri,  HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
+		ResponseEntity<List<Jeep>> response = 
+				getRestTemplate().exchange(uri, HttpMethod.GET, null,
+						new ParameterizedTypeReference<>() {});
 		 
 		//Then: a success(OK - 200) status code is returned
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);   
+		
+		//And: the actual list returned is the same as the expected list		
+		List<Jeep> expected = buildExpected(); 
+		
+		assertThat(response.getBody()).isEqualTo(expected); 
 	}
+		
 }
