48a2d9c4f632e9d96045f3fe9aec4f5e826b7722
OliGuzman <OlGuDev@gmail.com>
2022-08-20 06:39:27 -0500

    Added Week 4 Assignment

--- src/test/java/com/promineotech/jeep/controller/support/BaseTest.java
+++ src/test/java/com/promineotech/jeep/controller/support/BaseTest.java
@@ -1,5 +1,33 @@
 package com.promineotech.jeep.controller.support;
 
-public class BaseTest {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+//not sure if this was the correct import for LocalServerPort
 
+import lombok.Getter;
+
+abstract class BaseTest {
+	@LocalServerPort
+	protected int serverPort; 
+	
+	@Autowired
+	@Getter
+	protected TestRestTemplate restTemplate; 
+	
+	/**
+	 * 
+	 * @return
+	 */
+	protected String getBaseUriForJeeps() {
+		return String.format("http://localhost:%d/jeeps", serverPort); 
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	protected String getBaseUriForOrders() {
+		return String.format("http://localhost:%d/orders", serverPort); 
+	}
 }
